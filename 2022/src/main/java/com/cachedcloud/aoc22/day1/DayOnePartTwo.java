89fb4015e8df220fdbe2fbda4b218e66f65a514e
iTeije <iteije@outlook.com>
2023-12-01 19:49:27 +0100

    Complete 2022 day 1

--- 2022/src/main/java/com/cachedcloud/aoc22/day1/DayOnePartTwo.java
+++ 2022/src/main/java/com/cachedcloud/aoc22/day1/DayOnePartTwo.java
@@ -2,20 +2,22 @@ package com.cachedcloud.aoc22.day1;
 
 import com.cachedcloud.aoc.FileReader;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class DayOnePartOne {
+public class DayOnePartTwo {
 
     public static void main(String[] args) {
         FileReader reader = new FileReader("input-day1.txt");
         List<String> input = reader.getInputAsStrings();
 
         int runningCount = 0;
-        int max = 0;
+        List<Integer> totals = new ArrayList<>();
 
         for (String str : input) {
             if (str.equals("")) {
-                if (runningCount > max) max = runningCount;
+                totals.add(runningCount);
                 runningCount = 0;
                 continue;
             }
@@ -23,6 +25,12 @@ public class DayOnePartOne {
             runningCount += Integer.parseInt(str);
         }
 
-        System.out.println("Result (2022 D1P1): " + max);
+        // Sort and reverse list
+        Collections.sort(totals);
+        Collections.reverse(totals);
+
+        int total = totals.stream().limit(3).mapToInt(Integer::intValue).sum();
+
+        System.out.println("Result (2022 D1P2): " + total);
     }
 }
