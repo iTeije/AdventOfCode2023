fde22af3255025d169cb6d268777c1365d625aa7
iTeije <iteije@outlook.com>
2023-12-01 17:28:50 +0100

    Complete day 1 part 1

--- day-1/src/main/java/com/cachedcloud/aoc/DayOnePartOne.java
+++ day-1/src/main/java/com/cachedcloud/aoc/DayOnePartOne.java
@@ -1,9 +1,28 @@
 package com.cachedcloud.aoc;
 
+import java.util.List;
+
 public class DayOnePartOne {
 
     public static void main(String[] args) {
+        FileReader reader = new FileReader("input.txt");
+        List<String> input = reader.getInputAsStrings();
 
+        int sum = 0;
+
+        // Loop through all lines
+        for (String str : input) {
+            String extractedNumbers = str.replaceAll("[^0-9]", "");
+
+            // Get numbers
+            String twoDigitNumber = String.valueOf(extractedNumbers.charAt(0)) + extractedNumbers.charAt(extractedNumbers.length() - 1);
+
+            // Add to sum
+            sum += Integer.parseInt(twoDigitNumber);
+        }
+
+        // Print result
+        System.out.println("Result D1P1: " + sum);
     }
 
 }
