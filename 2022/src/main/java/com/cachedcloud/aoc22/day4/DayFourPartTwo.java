dc12f8560a2d60b37523534a714c8c19c4f06f4e
Teije <iteije@outlook.com>
2023-12-03 00:15:11 +0100

    Complete 2022 day 4

---
+++ 2022/src/main/java/com/cachedcloud/aoc22/day4/DayFourPartTwo.java
@@ -0,0 +1,43 @@
+package com.cachedcloud.aoc22.day4;
+
+import com.cachedcloud.aoc.FileReader;
+
+import java.util.List;
+
+public class DayFourPartTwo {
+
+    public static void main(String[] args) {
+        FileReader reader = new FileReader("input-day4.txt");
+        List<String> input = reader.getInputAsStrings();
+
+        int count = 0;
+        for (String line : input) {
+            String[] ranges = line.split(",");
+            Range firstRange = Range.from(ranges[0]);
+            Range secondRange = Range.from(ranges[1]);
+
+            if (firstRange.start >= secondRange.start && firstRange.start <= secondRange.start) {
+                count++;
+            } else if (firstRange.end >= secondRange.start && firstRange.end <= secondRange.end) {
+                count++;
+            } else if (secondRange.end >= firstRange.start && secondRange.end <= firstRange.end) count++;
+        }
+
+        System.out.println("Result (2022 D4P2): " + count);
+    }
+
+    private static class Range {
+        public final int start;
+        public final int end;
+
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public static Range from(String serialized) {
+            String[] data = serialized.split("-");
+            return new Range(Integer.parseInt(data[0]), Integer.parseInt(data[1]));
+        }
+    }
+}
