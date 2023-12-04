d256c6d5aae74ec4359ed9ddc5edf862e5bf9162
Teije <iteije@outlook.com>
2023-12-04 21:02:52 +0100

    Complete 2022 day 6

---
+++ 2022/src/main/java/com/cachedcloud/aoc22/day6/DaySixPartOne.java
@@ -0,0 +1,27 @@
+package com.cachedcloud.aoc22.day6;
+
+import com.cachedcloud.aoc.FileReader;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DaySixPartOne {
+
+    public static void main(String[] args) {
+        // Parse the single line of input
+        String input = new FileReader("input-day6.txt").getInputAsStrings().get(0);
+
+        for (int i = 4; i < input.length(); i++) {
+            String sub = input.substring(i - 4, i);
+
+            Set<Character> characters = sub.chars()
+                    .mapToObj(ch -> (char) ch)
+                    .collect(Collectors.toSet());
+
+            if (characters.size() == 4) {
+                System.out.println("Result (2022 D6P1): " + i);
+                break;
+            }
+        }
+    }
+}
---
+++ 2022/src/main/java/com/cachedcloud/aoc22/day6/DaySixPartTwo.java
@@ -0,0 +1,28 @@
+package com.cachedcloud.aoc22.day6;
+
+import com.cachedcloud.aoc.FileReader;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DaySixPartTwo {
+
+    public static void main(String[] args) {
+        // Parse the single line of input
+        String input = new FileReader("input-day6.txt").getInputAsStrings().get(0);
+        int distinctConsecutiveCount = 14;
+
+        for (int i = distinctConsecutiveCount; i < input.length(); i++) {
+            String sub = input.substring(i - distinctConsecutiveCount, i);
+
+            Set<Character> characters = sub.chars()
+                    .mapToObj(ch -> (char) ch)
+                    .collect(Collectors.toSet());
+
+            if (characters.size() == distinctConsecutiveCount) {
+                System.out.println("Result (2022 D6P2): " + i);
+                break;
+            }
+        }
+    }
+}
