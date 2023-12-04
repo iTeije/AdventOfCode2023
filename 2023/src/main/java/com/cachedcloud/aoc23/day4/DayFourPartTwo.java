8973d770dcc318e1aaef37da6ee14163c11c9784
Teije <iteije@outlook.com>
2023-12-04 16:42:05 +0100

    Concise 2023 D4P2 code

--- 2023/src/main/java/com/cachedcloud/aoc23/day4/DayFourPartTwo.java
+++ 2023/src/main/java/com/cachedcloud/aoc23/day4/DayFourPartTwo.java
@@ -3,9 +3,10 @@ package com.cachedcloud.aoc23.day4;
 import com.cachedcloud.aoc.FileReader;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DayFourPartTwo {
 
@@ -13,27 +14,19 @@ public class DayFourPartTwo {
         List<String> input = new FileReader("input-day4.txt").getInputAsStrings();
         int size = input.size();
 
-        Map<Integer, Integer> scratchCards = new HashMap<>();
+        // Generate a map with all scratch cards and the default amount of repetitions (which is 1)
+        Map<Integer, Integer> scratchCards = IntStream.rangeClosed(1, size).boxed()
+                .collect(Collectors.toMap(cardNumber -> cardNumber, reps -> 1));
 
         for (int i = 1; i <= size; i++) {
-            int repetitions = scratchCards.getOrDefault(i, 1);
-            if (repetitions == 1) scratchCards.put(i, 1);
+            // Remove string prefix and separate the winning numbers and the player card numbers
+            String[] parts = input.get(i - 1).substring(10).split(" \\| ");
 
-            String line = input.get(i - 1).substring(10);
-            String[] parts = line.split(" \\| ");
-
-            List<Integer> winningNumbers = Arrays.stream(parts[0].split(" "))
-                    .filter(str -> !str.equals(""))
-                    .map(Integer::parseInt)
-                    .toList();
-
-            List<Integer> myNumbers = Arrays.stream(parts[1].split(" "))
-                    .filter(str -> !str.equals(""))
-                    .map(Integer::parseInt)
-                    .toList();
+            List<Integer> winningNumbers = parse(parts[0]);
+            List<Integer> myNumbers = parse(parts[1]);
 
             long matches = winningNumbers.stream().filter(myNumbers::contains).count();
-            for (int rep = 0; rep < repetitions; rep++) {
+            for (int rep = 0; rep < scratchCards.get(i); rep++) {
                 for (int j = 0; j < matches; j++) {
                     int scratchCardAt = i + j + 1;
                     if (scratchCardAt > size) continue;
@@ -43,11 +36,17 @@ public class DayFourPartTwo {
             }
         }
 
-        int totalScore = scratchCards.values().stream().mapToInt(Integer::intValue).sum();
-        scratchCards.forEach((card, amount) -> {
-            System.out.println("Card " + card + " has " + amount + " repetitions");
-        });
+        // Log reps for debugging purposes
+        scratchCards.forEach((card, amount) -> System.out.println("Card " + card + " has " + amount + " repetitions"));
 
+        int totalScore = scratchCards.values().stream().mapToInt(Integer::intValue).sum();
         System.out.println("Result (2023 D4P2): " + totalScore);
+    }
+
+    private static List<Integer> parse(String input) {
+        return Arrays.stream(input.split(" "))
+                .filter(str -> !str.equals(""))
+                .map(Integer::parseInt)
+                .toList();
     }
 }
