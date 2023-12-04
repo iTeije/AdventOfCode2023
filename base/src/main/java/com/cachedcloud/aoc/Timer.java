565e4829ee96ca1605466cc2bad9e57289b8a134
Teije <iteije@outlook.com>
2023-12-04 16:58:39 +0100

    Add utility to print computing time

---
+++ base/src/main/java/com/cachedcloud/aoc/Timer.java
@@ -0,0 +1,20 @@
+package com.cachedcloud.aoc;
+
+/*
+ * Little utility class to print the time to compute
+ */
+public class Timer {
+
+    private static Long start = System.currentTimeMillis();
+    private static Long finish = System.currentTimeMillis();
+
+    public static void start() {
+        start = System.currentTimeMillis();
+    }
+
+    public static void finish() {
+        finish = System.currentTimeMillis();
+        System.out.println("Took " + (finish - start) + "ms to execute.");
+    }
+
+}
