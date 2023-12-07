99eaae3e45bb9b14c68e0a6bcaedd6ec14835868
Teije <iteije@outlook.com>
2023-12-07 11:56:12 +0100

    WIP day 7 part 2

--- 2023/src/main/java/com/cachedcloud/aoc23/day7/DaySevenPartTwo.java
+++ 2023/src/main/java/com/cachedcloud/aoc23/day7/DaySevenPartTwo.java
@@ -15,10 +15,11 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class DaySevenPartOne {
+public class DaySevenPartTwo {
 
-    private static final List<Character> cardWeights = new LinkedList<>(Arrays.asList('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'));
+    private static final List<Character> cardWeights = new LinkedList<>(Arrays.asList('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'));
 
     public static void main(String[] unusedArgs) {
         Timer.start();
@@ -58,13 +59,14 @@ public class DaySevenPartOne {
                 }
                 return 0;
             });
+            System.out.println(type.name() + ": " + list.stream().map(hand -> hand.hand).collect(Collectors.joining(", ")));
             for (Hand hand : list) {
                 totalWinnings.addAndGet((long) hand.bid * rank[0]);
                 rank[0]--;
             }
         });
 
-        System.out.println("Result (2023 D7P1): " + totalWinnings.get());
+        System.out.println("Result (2023 D7P2): " + totalWinnings.get());
         Timer.finish();
     }
 
@@ -86,14 +88,15 @@ public class DaySevenPartOne {
             for (char ch : hand.toCharArray()) {
                 characters.add(ch);
             }
-            return characters.size() == 1;
+            return characters.size() == 1 || (characters.size() == 2 && characters.contains('J'));
         }),
         FOUR_OF_A_KIND(5, hand -> {
             Map<Character, Integer> characters = new HashMap<>();
             for (char ch : hand.toCharArray()) {
                 characters.put(ch, characters.getOrDefault(ch, 0) + 1);
             }
-            return characters.containsValue(4);
+            int jokers = characters.getOrDefault('J', 0);
+            return characters.containsValue(4) || (jokers > 0 && characters.containsValue(4 - jokers));
         }),
         FULL_HOUSE(4, hand -> {
             Map<Character, Integer> characters = new HashMap<>();
@@ -101,14 +104,15 @@ public class DaySevenPartOne {
                 characters.put(ch, characters.getOrDefault(ch, 0) + 1);
             }
             // After four and five of a kind checks, this will suffice
-            return characters.size() == 2;
+            return characters.size() == 2 || (characters.size() == 3 && characters.containsKey('J'));
         }),
         THREE_OF_A_KIND(3, hand -> {
             Map<Character, Integer> characters = new HashMap<>();
             for (char ch : hand.toCharArray()) {
                 characters.put(ch, characters.getOrDefault(ch, 0) + 1);
             }
-            return characters.containsValue(3);
+            int jokers = characters.getOrDefault('J', 0);
+            return characters.containsValue(3) || (jokers > 0 && characters.containsValue(3 - jokers));
         }),
         TWO_PAIR(2, hand -> {
             Map<Character, Integer> characters = new HashMap<>();
@@ -119,6 +123,11 @@ public class DaySevenPartOne {
             for (int val : characters.values()) {
                 if (val == 2) pairCount++;
             }
+
+            int jokers = characters.getOrDefault('J', 0);
+            if (pairCount == 1 && jokers == 2) return true;
+            if (characters.values().stream().filter(val -> val == 1).count() == 2 && jokers == 2) return true;
+
             return pairCount == 2;
         }),
         ONE_PAIR(1, hand -> {
@@ -126,7 +135,8 @@ public class DaySevenPartOne {
             for (char ch : hand.toCharArray()) {
                 characters.put(ch, characters.getOrDefault(ch, 0) + 1);
             }
-            return characters.containsValue(2);
+            int jokers = characters.getOrDefault('J', 0);
+            return characters.containsValue(2) || jokers > 0;
         }),
         HIGH_CARD(0, hand -> true);
 
