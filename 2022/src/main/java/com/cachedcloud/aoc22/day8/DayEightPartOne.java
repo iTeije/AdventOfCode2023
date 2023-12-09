6357a97eb96fe60af231a82ca03fa491b956b0f1
Teije <iteije@outlook.com>
2023-12-09 02:13:26 +0100

    Complete 2022 day 8

---
+++ 2022/src/main/java/com/cachedcloud/aoc22/day8/DayEightPartOne.java
@@ -0,0 +1,97 @@
+package com.cachedcloud.aoc22.day8;
+
+import com.cachedcloud.aoc.FileReader;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class DayEightPartOne {
+
+    public static void main(String[] args) {
+        List<String> input = new FileReader("input-day8.txt").getInputAsStrings();
+
+        // Count horizontally first
+        Set<Coordinate> coordinates = new HashSet<>();
+        count(input, coordinates, false);
+
+        System.out.println(coordinates.size());
+
+        // Generate column lists
+        List<String> verticalInput = new ArrayList<>();
+        for (int i = 0; i < input.get(0).length(); i++) {
+            StringBuilder builder = new StringBuilder("");
+            for (String s : input) {
+                builder.append(s.charAt(i));
+            }
+            verticalInput.add(builder.toString());
+        }
+
+        // Count vertically
+        count(verticalInput, coordinates, true);
+
+        System.out.println("Result (2022 D8P1): " + coordinates.size());
+    }
+
+    public static void count(List<String> input, Set<Coordinate> coordinates, boolean vertical) {
+        int yIndex = 0;
+        // Check horizontally
+        for (String row : input) {
+            // Get list of numeric values for the input string
+            List<Integer> integerList = getNumericValues(row);
+
+            // Check from left to right/top to bottom
+            int currentHighest = -1;
+            int xIndex = 0;
+            for (int tree : integerList) {
+                if (tree > currentHighest) {
+                    currentHighest = tree;
+                    coordinates.add(vertical ? new Coordinate(yIndex, xIndex) : new Coordinate(xIndex, yIndex));
+                }
+                xIndex++;
+            }
+
+            // Reverse the list, so I can check from right to left or bottom to top
+            Collections.reverse(integerList);
+            currentHighest = -1;
+            xIndex = integerList.size() - 1;
+            for (int tree : integerList) {
+                if (tree > currentHighest) {
+                    currentHighest = tree;
+                    coordinates.add(vertical ? new Coordinate(yIndex, xIndex) : new Coordinate(xIndex, yIndex));
+                }
+                xIndex--;
+            }
+            yIndex++;
+        }
+    }
+
+    public static List<Integer> getNumericValues(String input) {
+        List<Integer> integerList = new ArrayList<>();
+        for (char ch : input.toCharArray()) {
+            integerList.add(Character.getNumericValue(ch));
+        }
+        return integerList;
+    }
+
+    @AllArgsConstructor
+    static class Coordinate {
+        public final int x;
+        public final int y;
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Coordinate other)) return false;
+            return other.x == this.x && other.y == this.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+}
---
+++ 2022/src/main/java/com/cachedcloud/aoc22/day8/DayEightPartTwo.java
@@ -0,0 +1,74 @@
+package com.cachedcloud.aoc22.day8;
+
+import com.cachedcloud.aoc.FileReader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DayEightPartTwo {
+
+    public static void main(String[] unusedArgs) {
+        List<String> input = new FileReader("input-day8.txt").getInputAsStrings();
+        List<List<Integer>> parsedInput = input.stream().map(DayEightPartTwo::getNumericValues).toList();
+
+        int lowerBound = 0;
+        int xUpperBound = input.get(0).length();
+        int yUpperBound = input.size();
+
+        int highestScenicScore = 0;
+
+        for (int x = 0; x < input.get(0).length(); x++) {
+            for (int y = 0; y < input.size(); y++) {
+                // X and Y are the coordinates of the tree
+                int treeHeight = parsedInput.get(y).get(x);
+
+                // Get distance to the right
+                int distRight = 0;
+                for (int runX = x + 1; runX < xUpperBound; runX++) {
+                    distRight++;
+                    if (parsedInput.get(y).get(runX) >= treeHeight) break;
+                }
+
+                // Get distance to the left
+                int distLeft = 0;
+                for (int runX = x - 1; runX >= lowerBound; runX--) {
+                    distLeft++;
+                    if (parsedInput.get(y).get(runX) >= treeHeight) break;
+                }
+
+                // Get distance up
+                int distUp = 0;
+                for (int runY = y - 1; runY >= lowerBound; runY--) {
+                    distUp++;
+                    if (parsedInput.get(runY).get(x) >= treeHeight) break;
+                }
+
+                // Get distance down
+                int distDown = 0;
+                for (int runY = y + 1; runY < yUpperBound; runY++) {
+                    distDown++;
+                    if (parsedInput.get(runY).get(x) >= treeHeight) break;
+                }
+
+                // Calculate score
+                int score = distRight * distLeft * distUp * distDown;
+                if (score > highestScenicScore) {
+                    highestScenicScore = score;
+                    System.out.println("New best score is now at coordinate x=" + x + ", y=" + y + " (height: " + treeHeight + ") with a score of " + score
+                            + " (r:" + distRight + ", l:" + distLeft + ", u:" + distUp + ", d:" + distDown + ")");
+                }
+            }
+        }
+
+        System.out.println("Result (2022 D8P2): " + highestScenicScore);
+    }
+
+    public static List<Integer> getNumericValues(String input) {
+        List<Integer> integerList = new ArrayList<>();
+        for (char ch : input.toCharArray()) {
+            integerList.add(Character.getNumericValue(ch));
+        }
+        return integerList;
+    }
+}
