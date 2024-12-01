07bc191e692195560632d81a8c5e8b1241a9bd26
Teije <iteije@outlook.com>
2024-12-01 14:46:40 +0100

    Prepare day two

---
+++ 2024/src/main/java/com/cachedcloud/aoc24/day2/DayTwoPartTwo.java
@@ -0,0 +1,17 @@
+package com.cachedcloud.aoc24.day2;
+
+import com.cachedcloud.aoc.FileReader;
+
+import java.util.List;
+
+public class DayTwoPartTwo {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader("input-day2.txt");
+        List<String> input = fileReader.getInputAsStrings();
+
+
+
+    }
+
+}
