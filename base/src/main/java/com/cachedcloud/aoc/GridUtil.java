08fc3c8fe2001b221ea5695e8fe3ca5398db85be
Teije <iteije@outlook.com>
2024-12-10 13:34:43 +0100

    Add util function to parse input as 2D int array

--- base/src/main/java/com/cachedcloud/aoc/GridUtil.java
+++ base/src/main/java/com/cachedcloud/aoc/GridUtil.java
@@ -13,6 +13,16 @@ public class GridUtil {
         return grid;
     }
 
+    public static int[][] createIntGrid(List<String> input) {
+        int[][] grid = new int[input.size()][input.get(0).length()];
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(i).length(); j++) {
+                grid[i][j] = Character.getNumericValue(input.get(i).charAt(j));
+            }
+        }
+        return grid;
+    }
+
     public static void print(char[][] grid) {
         List<String> list = new ArrayList<>();
         for (char[] chars : grid) {
@@ -23,6 +33,18 @@ public class GridUtil {
             list.add(builder.toString());
         }
 
+        list.forEach(System.out::println);
+    }
+
+    public static void print(int[][] grid) {
+        List<String> list = new ArrayList<>();
+        for (int[] ints : grid) {
+            StringBuilder builder = new StringBuilder();
+            for (int anInt : ints) {
+                builder.append(anInt);
+            }
+            list.add(builder.toString());
+        }
         list.forEach(System.out::println);
     }
 
