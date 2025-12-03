844cc00672ca48c2d7c3ea0923a6036e154c08f8
Teije <iteije@outlook.com>
2025-12-03 17:21:52 +0100

    [2025] Complete day 3 (not garbage code)

--- 2025/src/main/java/com/cachedcloud/aoc25/day3/DayThreePartOne.java
+++ 2025/src/main/java/com/cachedcloud/aoc25/day3/DayThreePartOne.java
@@ -2,57 +2,45 @@ package com.cachedcloud.aoc25.day3;
 
 import com.cachedcloud.aoc.common.FileReader;
 
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DayThreePartOne {
 
     public static void main(String[] args) {
         FileReader reader = new FileReader("input-day3.txt");
 //        FileReader reader = new FileReader("example-input-day3.txt");
-        List<String> input = reader.getInputAsStrings();
 
-        int count = 0;
-        for (String line : input) {
-            int firstHighest = 0;
-            int highestIndex = 0;
-            for (int i = 0; i < line.length(); i++) {
-                char c = line.charAt(i);
-                int intValue = c - '0';
+        AtomicInteger count = new AtomicInteger();
+        reader.getInputAsStrings().forEach(s -> {
+            count.addAndGet(Integer.parseInt(jottage(s, 2)));
+        });
 
-                if (intValue > firstHighest) {
-                    firstHighest = intValue;
+        System.out.println("D3P1: " + count);
+    }
+
+    public static String jottage(String input, int batteries) {
+        StringBuilder builder = new StringBuilder();
+        int startIndex = 0;
+
+        while (batteries > 0) {
+            int highest = 0;
+            int highestIndex = startIndex;
+            for (int i = startIndex; i < input.length() - batteries + 1; i++) {
+                char charAt = input.charAt(i);
+                int intValue = charAt - '0';
+                if (intValue > highest) {
+                    highest = intValue;
                     highestIndex = i;
                 }
             }
 
-            int secondHighest = 0;
-            int secondIndex = 0;
-            boolean isLast = highestIndex == line.length() - 1;
+            builder.append(highest);
+            startIndex = highestIndex + 1;
 
-            for (int i = 0; i < line.length(); i++) {
-                if (i == highestIndex) continue;
-
-                char c = line.charAt(i);
-                int intValue = c - '0';
-
-                if (intValue > secondHighest && (isLast || i > highestIndex || intValue >= firstHighest)) {
-                    secondHighest = intValue;
-                    secondIndex = i;
-                }
-            }
-
-            if (secondIndex > highestIndex) {
-                System.out.println("s>h " + firstHighest + secondHighest);
-                count += Integer.parseInt(String.valueOf(firstHighest + "" + secondHighest));
-            } else {
-                System.out.println("s<h " + secondHighest + firstHighest);
-                count += Integer.parseInt(String.valueOf(secondHighest + "" + firstHighest));
-            }
+            batteries--;
         }
 
-
-
-        System.out.println("D3P1: " + count);
+        return builder.toString();
     }
 
 }
